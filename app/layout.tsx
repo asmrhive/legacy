65882b4308f2d38718afc18868851c96e3e04264
Hynek Fišera <hynek.fisera@seznam.cz>
2026-02-01 17:59:55 +0100

    Update domain & remove GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import {Inter, Inter_Tight} from "next/font/google";
 import { Metadata } from "next";
 import Link from "next/link";
-import GAWrapper from "./GAWrapper";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
@@ -16,7 +15,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://www.asmrhive.com"),
+  metadataBase: new URL("https://legacy.asmrhive.com"),
   title: "ASMR Hive",
   description: 'Website inspired by "this or that" test from YouTube video by @TiptoeTingles and @GibiASMR. This website allows you to try this test out by yourself.',
 };
@@ -40,7 +39,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="apple-mobile-web-app-title" content="ASMR Hive" />
       </Head>
       <body className={`font-sans ${inter.variable} ${interTight.variable} antialiased`}>
-        <GAWrapper />
         <header className="absolute top-0 left-0 right-0 z-50 p-4 flex flex-row justify-start">
           <Link href="/" className="select-none opacity-90 hover:opacity-100 transition-opacity duration-200">
             <Image src={Logo} alt={`ASMR Hive logo`} className="h-8 w-auto" />
